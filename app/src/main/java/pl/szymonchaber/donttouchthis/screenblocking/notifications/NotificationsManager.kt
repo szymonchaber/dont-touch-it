46551f7ce4487c3fb77a9b969b732ecda465aaf8
Szymon Chaber <szymon.chaber@o2.pl>
2018-04-12 10:45:08 +0200

    fix app for Android Oreo

--- app/src/main/java/pl/szymonchaber/donttouchthis/screenblocking/notifications/NotificationsManager.kt
+++ app/src/main/java/pl/szymonchaber/donttouchthis/screenblocking/notifications/NotificationsManager.kt
@@ -1,10 +1,12 @@
 package pl.szymonchaber.donttouchthis.screenblocking.notifications
 
-import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.support.v4.app.NotificationCompat
 import pl.szymonchaber.donttouchthis.MainActivity
 import pl.szymonchaber.donttouchthis.R
 import pl.szymonchaber.donttouchthis.screenblocking.OverlayService
@@ -13,17 +15,30 @@ internal class NotificationsManager(private val context: Context, private val li
 
     private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-    private val notificationBuilder: Notification.Builder
+    private val notificationBuilder: NotificationCompat.Builder
 
     init {
-        val action = Notification.Action.Builder(R.drawable.ic_settings_black_24dp,
-                context.getString(R.string.notification_settings),
-                PendingIntent.getActivity(context, 0, Intent(context, MainActivity::class.java), 0)).build()
-        notificationBuilder = Notification.Builder(context).setSmallIcon(android.R.drawable.btn_star)
-                .setContentTitle(context.getString(R.string.app_name))
-                .setContentIntent(notificationClickedIntent())
-                .setDeleteIntent(notificationDeletedIntent())
-                .addAction(action)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(
+                NotificationChannel(
+                    CHANNEL_ID, context.getString(R.string.channel_name),
+                    NotificationManager.IMPORTANCE_LOW
+                )
+            )
+        }
+
+        val action = NotificationCompat.Action.Builder(
+            R.drawable.ic_settings_black_24dp,
+            context.getString(R.string.notification_settings),
+            PendingIntent.getActivity(context, 0, Intent(context, MainActivity::class.java), 0)
+        ).build()
+
+        notificationBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.btn_star)
+            .setContentTitle(context.getString(R.string.app_name))
+            .setContentIntent(notificationClickedIntent())
+            .setDeleteIntent(notificationDeletedIntent())
+            .addAction(action)
     }
 
     fun consumeAction(action: String?) {
@@ -37,7 +52,8 @@ internal class NotificationsManager(private val context: Context, private val li
     }
 
     fun notify(message: String) = notificationManager.notify(
-            ID, notificationBuilder.setContentText(message).build())
+        ID, notificationBuilder.setContentText(message).build()
+    )
 
     private fun notificationClickedIntent(): PendingIntent {
         val intent = Intent(context, OverlayService::class.java).apply {
@@ -58,5 +74,6 @@ internal class NotificationsManager(private val context: Context, private val li
         private const val ID = 1
         private const val CLICK = "CLICK"
         private const val DISMISS = "DISMISS"
+        private const val CHANNEL_ID = "main_channel_id"
     }
 }
--- app/src/main/java/pl/szymonchaber/donttouchthis/screenblocking/view/BackBlockingFrameLayout.kt
+++ app/src/main/java/pl/szymonchaber/donttouchthis/screenblocking/view/BackBlockingFrameLayout.kt
@@ -2,12 +2,14 @@ package pl.szymonchaber.donttouchthis.screenblocking.view
 
 import android.content.Context
 import android.graphics.PixelFormat
+import android.os.Build
 import android.support.annotation.AttrRes
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.KeyEvent
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.view.WindowManager.LayoutParams
 import android.widget.FrameLayout
 
 class BackBlockingFrameLayout : FrameLayout {
@@ -21,14 +23,22 @@ class BackBlockingFrameLayout : FrameLayout {
     override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
         return when (event?.keyCode) {
             KeyEvent.KEYCODE_BACK -> true
-            else                  -> super.dispatchKeyEvent(event)
+            else -> super.dispatchKeyEvent(event)
         }
     }
 
     fun createParams(): ViewGroup.LayoutParams {
-        return WindowManager.LayoutParams(
-            WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT,
-            WindowManager.LayoutParams.TYPE_PHONE, WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        val layoutType = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else {
+            WindowManager.LayoutParams.TYPE_PHONE
+        }
+
+        return LayoutParams(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.MATCH_PARENT,
+            layoutType,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
             PixelFormat.TRANSLUCENT
         ).apply {
             gravity = Gravity.CENTER
